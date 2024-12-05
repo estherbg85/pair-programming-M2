4dd1c74c12f7576303d00649bfbddd2cb06f387d
estherbg85 <estherbonillagirol85@gmail.com>
2024-12-05 21:50:32 +0100

    changes

--- Piedra, papel y tijera/main.js
+++ Piedra, papel y tijera/main.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const submit = document.querySelector('.js_submit');
-const startGame = document.querySelector('.js_startGame');
 
+function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  }
+
+
+const submit = document.querySelector('.js_submit');
+const selectGame = document.querySelector('.js_selectGame');
+
+
+const jugadaFinal = () => {
+    const randomNumber = getRandomNumber (9);
+
+
+if (RandomNumber <=3) return'Piedra' {
+
+selectGame.innerHTML = `<p>
+   
+}
+
+
+
+}
 submit.addEventListener('click', (ev) => {
     ev.preventDefault();
 
 const jugada = document.querySelector('.js_jugada').value;
 
+
+
 });
 
-function getRandomNumber(max) { 
-    return Math.ceil(Math.random() * max); 
-  }
