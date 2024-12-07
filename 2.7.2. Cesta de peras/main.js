958732b42e178e3e2b83ff0d66665cf4c009fb61
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-07 13:02:16 +0100

    Add SacarPera part

--- 2.7.2. Cesta de peras/main.js
+++ 2.7.2. Cesta de peras/main.js
@@ -1,2 +1,51 @@
 'use strict';
 
+let cesta = {
+
+max: 50,
+min: 10,
+current: 25,
+initial: 15,
+
+sacarUna: () => {
+if (cesta.current > cesta.min) {
+
+cesta.current-=1;
+}
+},
+
+sacarVarias: (cuantas) => {  // cuantas = 13
+    if( cesta.current-cuantas >= cesta.min) {
+cesta.current -= cuantas;
+    }
+}
+
+añadirUna: () => {
+    if (cesta.current <= cesta.max) {
+        cesta.current+=1;
+    }
+
+        
+}
+};
+
+
+// {max: 50, min: 10, current: 25, initial: 15, sacarUna: ƒ(), …}
+cesta.sacarVarias(2);
+// {max: 50, min: 10, current: 23, initial: 15, sacarUna: ƒ(), …}
+console.log(cesta);
+
+cesta.sacarVarias(13);
+// {max: 50, min: 10, current: 10, initial: 15, sacarUna: ƒ(), …}
+console.log(cesta);
+
+cesta.sacarUna(1);
+
+console.log(cesta);
+
+cesta.sacarUna();
+
+console.log(cesta);
+
+
+
