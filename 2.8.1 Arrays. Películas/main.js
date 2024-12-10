419f631d4bcba269ca3bf48bf8756e2b9e6c71a3
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-10 21:32:45 +0100

    Add movies exercise arrays

---
+++ 2.8.1 Arrays. Películas/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const workWithMovies = () => {
+
+    debugger;
+
+const favoriteMovies = [
+    `Con faldas y a lo loco`,
+    `Grease`,
+    `Tomates verdes fritos`,
+    `Amélie`,
+
+];
+
+ favoriteMovies.push (`Charlie y la fábrica de chocolate`);
+
+
+ console.log(favoriteMovies);
+
+ 
+};
+
+workWithMovies();
+
+
+ 
+
