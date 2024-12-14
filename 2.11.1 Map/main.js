ce74a91d4f1e331d1c34ff2f87602da84e77b08c
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-14 14:05:36 +0100

    Add exercise user premium

--- 2.11.1 Map/main.js
+++ 2.11.1 Map/main.js
@@ -3,19 +3,51 @@
 
 // Inflar las notas
 
-/*
 const marks = [5, 4, 6, 7, 9];
 
 const inflatedMarks = marks.map((mark) => mark + 1);
 
 console.log(inflatedMarks);
-*/
 
 
 // Saludar es de buena educación
 
 const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada']; 
 
-const greetingNames = names.map((name) => 'Bienvenida + name' );
+const greetingNames = names.map((name) => `Bienvenida, ${name}` );
 
 console.log(greetingNames);
+
+
+// Gracias por confiar en nosotros
+
+const users = [
+    { name: 'María', isPremium: false },
+    { name: 'Lucía', isPremium: true },
+    { name: 'Susana', isPremium: true },
+    { name: 'Rocío', isPremium: false },
+    { name: 'Inmaculada', isPremium: false },
+];
+
+
+const greetings = users.map((oneUser) => {
+    if(oneUser.isPremium === true) {
+        return `Bienvenida, ${oneUser.name}. Gracias por confiar en nosotras.`;
+
+    }
+    else {
+        return `Bienvenida, ${oneUser.name}.`;
+
+    }
+
+});
+
+console.log(greetings);
+
+
+/* Esta es la explicación que nos ha dado Iván de que todos los métodos funcionales de array (como map) llevan implícito un returm
+
+const greetings2implicito = users.map( oneUser => `Bienvenida, ${oneUser.name}` );
+const greetings2explicito = users.map((oneUser) => { return `Bienvenida, ${oneUser.name}`; } );
+
+*/
