f8de12814826f26aa4f3167c260e70069c832093
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-14 12:59:36 +0100

    Add map exercises

---
+++ 2.11.1 Map/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// Inflar las notas
+
+/*
+const marks = [5, 4, 6, 7, 9];
+
+const inflatedMarks = marks.map((mark) => mark + 1);
+
+console.log(inflatedMarks);
+*/
+
+
+// Saludar es de buena educación
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada']; 
+
+const greetingNames = names.map((name) => 'Bienvenida + name' );
+
+console.log(greetingNames);
